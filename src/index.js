19f30839e5c967990f0846c0b8a946e0dd2731fe
ErwannLG <erwannj.legall@gmail.com>
2022-04-19 16:40:57 +0200

    redux toolkit

--- src/index.js
+++ src/index.js
@@ -1,23 +1,19 @@
 import ReactDOM from 'react-dom'
-import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
+import { configureStore } from '@reduxjs/toolkit'
 import App from './App'
-import noteReducer, { createNote } from './reducers/noteReducer'
-import filterReducer from './reducers/filterReducer'
-import { filterChange } from './reducers/filterReducer'
 
-const reducer = combineReducers({
-  notes: noteReducer,
-  filter: filterReducer
+import noteReducer from './reducers/noteReducer'
+import filterReducer from './reducers/filterReducer'
+
+const store = configureStore({
+  reducer: {
+    notes: noteReducer,
+    filter: filterReducer
+  }
 })
 
-const store = createStore(reducer)
-
 console.log(store.getState())
-
-store.subscribe(() => console.log(store.getState()))
-store.dispatch(filterChange('IMPORTANT'))
-store.dispatch(createNote('combineReducers forms one reducer from many simple reducers'))
 
 ReactDOM.render(
   <Provider store={store}>
