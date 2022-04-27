b8c847a00eaaba3baa32f2867fcb4102949583ad
ErwannLG <erwannj.legall@gmail.com>
2022-04-27 15:51:03 +0200

    communicating with server in a redux app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { useEffect } from 'react'
 import NewNote from './components/NewNote'
 import Notes from './components/Notes'
-import VisibilityFilter from './components/VisibilityFilter';
+import VisibilityFilter from './components/VisibilityFilter'
+import noteService from './services/notes'
+import { initializeNotes, setNotes } from './reducers/noteReducer'
+import { useDispatch } from 'react-redux'
 
 
 const App = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(initializeNotes())
+  }, [dispatch])
+
   return (
     <div>
       <NewNote />
